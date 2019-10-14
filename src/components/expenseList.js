5ef3d21e115dbc7441a98e3d000dd1e73e077bca
josephprasanth <prasanthjoseph_77@yahoo.com>
2019-10-14 17:47:51 +0200

    Sucessfully added sum with corresponding styling

--- src/components/expenseList.js
+++ src/components/expenseList.js
@@ -1,35 +1,36 @@
 import React from 'react';
 
-const ExpenseList = ({ expenses,currency }) => {
+const ExpenseList = ({ expenses, currency }) => {
     let sum = 0;
-    const expensesListEl = expenses.map((expensez, index) => {
-        sum += expensez.amount *1;
+    const expensesListEl = expenses.map((expense, index) => {
+        sum += expense.amount * 1;
         return (
             <div key={index} className="list-group-item">
-            <div className="d-flex">
-            <h5>{expensez.comment}</h5>
-            <strong>
-            { expensez.amount }
-            { currency }
-            </strong>
-            </div>            
+                <div className="d-flex w-100 justify-content-between">
+                    <h5 className="mb-1">{expense.comment}</h5>
+                    <strong>
+                        {expense.amount}
+                        {currency}
+                    </strong>
+                </div>
             </div>
         )
     })
 
-    return ( <div>{ expensesListEl }</div> )
-    //     return (  
-    //     <div>{expenses.map((res) => {
-    //         return (
-    //             <div>
-    //             <li>{res.amount}</li>
-    //             <li>{res.comment}</li>
-    //             </div>
-    //         )
-                
-            
-    //     })}</div>
-    // );
-}
- 
+    return (
+        <> 
+        { expenses.length > 0 ? (
+            <>
+            <h4>Sum: {sum}</h4>
+            <div className="list-group mt-3">
+            {expensesListEl}
+            </div>
+            </>
+            ) : (
+                <span>No data yet</span>    
+            )}        
+        </>
+    );
+};
+
 export default ExpenseList;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootswatch/dist/lumen/bootstrap.min.css";
 import App from './components/app';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
