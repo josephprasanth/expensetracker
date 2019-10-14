6bf677c589b81102850f56d4f5904ace46996438
josephprasanth <prasanthjoseph_77@yahoo.com>
2019-10-14 15:53:42 +0200

    Since developement would take more time with only regular css and bem naming  , I am using bootswatch to rapidly deploy project with bootstrap 4 syntax

--- src/components/createExpenseForm.js
+++ src/components/createExpenseForm.js
@@ -1,75 +1,78 @@
 import React, { Component } from 'react';
 
+const initialState = {
+    error: [],
+    expense: {
+        amount: '',
+        comment: ''
+    }
+}
+
 class CreateExpenseForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            error: [],
-            expense: {
-                amount: '',
-                comment: ''
-            }
-        }
+        this.state = initialState;
     }
-    
+
     onFieldChange = (e) => {
-          let el = e.target;
-          this.setState({
-              expense: {
-                ...this.state.expense, [el.id] : el.value
-              }
-          }, () => console.log(this.state)
-          );
+        let el = e.target;
+        this.setState({
+            expense: {
+                ...this.state.expense, [el.id]: el.value
+            }
+        }, () => console.log(this.state)
+        );
     }
 
     onSubmitClick = () => {
-       // Doing Validation 
-       let { amount,comment } = this.state.expense;
-       if (!amount || !comment) {
-           this.setState({ 
-               error: ["Invalid Input"]
-           })
-           console.log(this.state.error);
-           
-       }
-       
+        // Doing Validation 
+        let { amount } = this.state.expense;
+        if (!amount || isNaN(amount)) {
+            this.setState({
+                error: ["Invalid Input"]
+            })
+        }
+        this.props.onCreate(this.state.expense);
+        this.setState(initialState);
     }
 
 
     render() {
-        const { expense,error } = this.state;
+        const { expense, error } = this.state;
         const { onCreate } = this.props;
+        //console.log(expense["amount"]);
+
         return (
             <>
                 <div className="cef">
                     <div className="cef--errors">
-                       {error && error.map((e) => {
-                           return <p>{e}</p>
-                       })};
+                        {error && error.map((e, index) => {
+                            return <p key={index}>{e}</p>
+                        })}
                     </div>
                     <div className="cef--field">
                         <label>Amount</label>
-                        <input 
-                        id="amount" 
-                        type="Number"
-                        placeholder="eg 300"
-                        onChange={this.onFieldChange}
-                        value={expense["amount"]}  
+                        <input
+                            id="amount"
+                            type="Number"
+                            placeholder="eg 300"
+                            onChange={this.onFieldChange}
+                            value={expense["amount"]}
                         />
                     </div>
                     <div className="cef--field">
                         <label>Comment</label>
-                        <input 
-                        id="comment"                       
-                        type="text"
-                        placeholder="eg Restaurant" 
-                        onChange={this.onFieldChange}
-                        value={expense["comment"]} 
+                        <input
+                            id="comment"
+                            type="text"
+                            placeholder="eg Restaurant"
+                            onChange={this.onFieldChange}
+                            value={expense["comment"]}
                         />
                     </div>
                     <button onClick={this.onSubmitClick}>Submit</button>
                 </div>
-               
+
             </>
 
         );
