ad529818054a055f378eb2c6235c937bd9d85e17
josephprasanth <prasanthjoseph_77@yahoo.com>
2019-10-14 17:27:56 +0200

    Roughly rendering the expense List - Have to add more styling and the sum feature

--- src/components/expenseList.js
+++ src/components/expenseList.js
@@ -2,13 +2,34 @@ import React from 'react';
 
 const ExpenseList = ({ expenses,currency }) => {
     let sum = 0;
-    
+    const expensesListEl = expenses.map((expensez, index) => {
+        sum += expensez.amount *1;
+        return (
+            <div key={index} className="list-group-item">
+            <div className="d-flex">
+            <h5>{expensez.comment}</h5>
+            <strong>
+            { expensez.amount }
+            { currency }
+            </strong>
+            </div>            
+            </div>
+        )
+    })
 
-    return (  
-        <div>{expenses.map((res) => {
-            return <li>{res.amount}</li>
-        })}</div>
-    );
+    return ( <div>{ expensesListEl }</div> )
+    //     return (  
+    //     <div>{expenses.map((res) => {
+    //         return (
+    //             <div>
+    //             <li>{res.amount}</li>
+    //             <li>{res.comment}</li>
+    //             </div>
+    //         )
+                
+            
+    //     })}</div>
+    // );
 }
  
 export default ExpenseList;
