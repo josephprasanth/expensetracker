6835eb272605bb8c0f596683487008f02bcbb22c
josephprasanth <prasanthjoseph_77@yahoo.com>
2019-10-14 20:52:37 +0200

    Corrected and tested error functionality on app , so it is now working for invalid inputs

--- src/components/createExpenseForm.js
+++ src/components/createExpenseForm.js
@@ -20,33 +20,37 @@ class CreateExpenseForm extends Component {
             expense: {
                 ...this.state.expense, [el.id]: el.value
             }
-        }, () => console.log(this.state)
-        );
+        });
     }
 
     onSubmitClick = () => {
-        // Doing Validation 
-        let { amount } = this.state.expense;
+        let { expense, expense: { amount }, errors } = this.state;
         if (!amount || isNaN(amount)) {
             let error = "Invalid Input";
-            this.setState({
-                errors: [error]
-            })
+            //If this error is already there , then dont add it
+            if (errors.indexOf(error) < 0) {
+                this.setState({
+                    errors: [...errors, error]
+                });
+            }
+            return;
         }
-        this.props.onCreate(this.state.expense);
+        this.props.onCreate(expense);
         this.setState(initialState);
-    }
-
+    };
 
     render() {
-        const { expense, error } = this.state;
+        const { expense, errors } = this.state;
         return (
             <>
                 <fieldset>
                     <legend>Create Expense</legend>
 
-                    {error && error.map((e, index) => {
-                        return <p key={index}>{e}</p>
+                    {errors && errors.map((e, index) => {
+                        return (
+                            <div className="alert alert-warning" key={index}>
+                                {e}
+                            </div>)
                     })}
 
                     <div className="form-group">
